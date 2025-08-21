06f91b58733e7cdc4a8db8f550e2d12a5fc7de81
vienora <sales.vienora@gmail.com>
2025-08-21 01:01:57 -0400

    Delete src/app directory

--- src/app/admin/suppliers/page.tsx
+++
@@ -1,9 +0,0 @@
-import SupplierDashboard from '@/components/SupplierDashboard';
-
-export default function SuppliersAdminPage() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <SupplierDashboard />
-    </div>
-  );
-}
